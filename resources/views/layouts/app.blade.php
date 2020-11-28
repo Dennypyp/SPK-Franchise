645d8ba7f51a17042114f40e87463a4a000ef37d
Dennypyp <denny.pyp11@gmail.com>
2020-11-29 00:21:50 +0700

    Mempercantik home user (menambahkan alur)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,13 @@
             .brand img {
                 width: 100%;
             }
+            
+            .homeimg{
+            width: 120px;
+            height: 110px;
+            overflow: hidden;
+            margin: 10px auto;
+            }
     </style>
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
